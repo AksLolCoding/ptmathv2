3156c446c13cd8133570befbc2e91249e9a2a9a8
Username93939 <128490471+Username93939@users.noreply.github.com>
2026-02-12 13:00:12 +0000

    set up node stuff and express

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ptmath
+yo ima go set up the stuff for node now bc why not
---
+++ index.js
@@ -0,0 +1,24 @@
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+import { createRequire } from "module"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const require = createRequire(import.meta.url);
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.listen(3000, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Port 3000");
+});
