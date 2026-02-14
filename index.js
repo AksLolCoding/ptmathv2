86c76ee62869063438d3b836bed434007f01d30b
Username93939 <128490471+Username93939@users.noreply.github.com>
2026-02-14 20:25:30 +0000

    created pages and structure for navigation bar

--- index.js
+++ index.js
@@ -16,7 +16,26 @@ app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
-//sends error-404.html when the user gets a 404 error
+app.get("/events", (req, res) => {
+    res.sendFile(`${__dirname}/public/events.html`);
+});
+
+app.get("/calendar", (req, res) => {
+    res.sendFile(`${__dirname}/public/calendar.html`);
+});
+
+app.get("/news", (req, res) => {
+    res.sendFile(`${__dirname}/public/news.html`);
+});
+
+app.get("/resources", (req, res) => {
+    res.sendFile(`${__dirname}/public/resources.html`);
+});
+
+app.get("/team", (req, res) => {
+    res.sendFile(`${__dirname}/public/team.html`);
+});
+
 app.use((req, res, next) => {
     res.status(404).sendFile(`${__dirname}/public/error-404.html`);
 });
